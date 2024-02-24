32514ba8ea63b4a1c4419a643a05858e8cd18a32
Chai Landau <clandau@octoml.ai>
2024-02-23 19:45:18 -0500

    feat: add styles for footer credit, page content * refactor: fix heading margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,93 +11,96 @@ export default function Home() {
         current="January 25, 2023"
         nextText="Jan 26, 2023"
       />
-      <CardGrid
-        heading={
-          <h1>
-            10 Most <br />
-            <span>Popular</span> Models
-          </h1>
-        }
-        cards={[
-          {
-            icon: "/models/mistral.webp",
-            modelAuthor: "stabilityai",
-            modelName: "japanese-stablelm-instruct-alpha-7b",
-            externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-          },
-          {
-            icon: "/models/mistral.webp",
-            modelAuthor: "mistralai",
-            modelName: "Mistral-7B-v0.1",
-            externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-          },
-          {
-            icon: "/models/mistral.webp",
-            modelAuthor: "mistralai",
-            modelName: "Mistral-7B-v0.1",
-            externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-          },
-          {
-            icon: "/models/mistral.webp",
-            modelAuthor: "mistralai",
-            modelName: "Mistral-7B-v0.1",
-            externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-          },
-          {
-            icon: "/models/mistral.webp",
-            modelAuthor: "mistralai",
-            modelName: "Mistral-7B-v0.1",
-            externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-          },
-          {
-            icon: "/models/mistral.webp",
-            modelAuthor: "mistralai",
-            modelName: "Mistral-7B-v0.1",
-            externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-          },
-          {
-            icon: "/models/mistral.webp",
-            modelAuthor: "mistralai",
-            modelName: "Mistral-7B-v0.1",
-            externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-          },
-          {
-            icon: "/models/mistral.webp",
-            modelAuthor: "mistralai",
-            modelName: "Mistral-7B-v0.1",
-            externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-          },
-          {
-            icon: "/models/mistral.webp",
-            modelAuthor: "mistralai",
-            modelName: "Mistral-7B-v0.1",
-            externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-          },
-          {
-            icon: "/models/mistral.webp",
-            modelAuthor: "mistralai",
-            modelName: "Mistral-7B-v0.1",
-            externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-          },
-        ]}
-      />
-      <Switchback
-        leftContent={
-          <h2 className="heading">
-            <span>45,515</span> text gen
-            <br /> models existed
-          </h2>
-        }
-        rightContent={
-          <p>
-            We like to say that in AI, a week is a month and a month is a year,
-            and the "latest and greatest model" changes just as quickly. We
-            tracked the most popular text-gen models starting in January 2023 to
-            highlight the rapid rate of change in the AI industry. Click through
-            time to see how quickly the state-of-the-art changes.
-          </p>
-        }
-      />
+      <div className="page-content">
+        <CardGrid
+          heading={
+            <h1>
+              10 Most <br />
+              <span>Popular</span> Models
+            </h1>
+          }
+          cards={[
+            {
+              icon: "/models/mistral.webp",
+              modelAuthor: "stabilityai",
+              modelName: "japanese-stablelm-instruct-alpha-7b",
+              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
+            },
+            {
+              icon: "/models/mistral.webp",
+              modelAuthor: "mistralai",
+              modelName: "Mistral-7B-v0.1",
+              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
+            },
+            {
+              icon: "/models/mistral.webp",
+              modelAuthor: "mistralai",
+              modelName: "Mistral-7B-v0.1",
+              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
+            },
+            {
+              icon: "/models/mistral.webp",
+              modelAuthor: "mistralai",
+              modelName: "Mistral-7B-v0.1",
+              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
+            },
+            {
+              icon: "/models/mistral.webp",
+              modelAuthor: "mistralai",
+              modelName: "Mistral-7B-v0.1",
+              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
+            },
+            {
+              icon: "/models/mistral.webp",
+              modelAuthor: "mistralai",
+              modelName: "Mistral-7B-v0.1",
+              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
+            },
+            {
+              icon: "/models/mistral.webp",
+              modelAuthor: "mistralai",
+              modelName: "Mistral-7B-v0.1",
+              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
+            },
+            {
+              icon: "/models/mistral.webp",
+              modelAuthor: "mistralai",
+              modelName: "Mistral-7B-v0.1",
+              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
+            },
+            {
+              icon: "/models/mistral.webp",
+              modelAuthor: "mistralai",
+              modelName: "Mistral-7B-v0.1",
+              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
+            },
+            {
+              icon: "/models/mistral.webp",
+              modelAuthor: "mistralai",
+              modelName: "Mistral-7B-v0.1",
+              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
+            },
+          ]}
+        />
+        <Switchback
+          leftContent={
+            <h2 className="heading">
+              <span>45,515</span> text gen
+              <br /> models existed
+            </h2>
+          }
+          rightContent={
+            <p>
+              We like to say that in AI, a week is a month and a month is a
+              year, and the "latest and greatest model" changes just as quickly.
+              We tracked the most popular text-gen models starting in January
+              2023 to highlight the rapid rate of change in the AI industry.
+              Click through time to see how quickly the state-of-the-art
+              changes.
+            </p>
+          }
+        />
+      </div>
     </main>
   );
 }
