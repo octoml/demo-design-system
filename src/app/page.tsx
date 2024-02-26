453e2ff35df4861c4ae861ec50d6837b95af87dc
Chai Landau <clandau@octoml.ai>
2024-02-26 10:32:25 -0500

    feat: add page not found styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,102 +5,13 @@ import Switchback from "@/components/Switchback";
 
 export default function Home() {
   return (
-    <main className="container wide">
-      <ContentNav
-        prevText="Jan 24, 2023"
-        current="January 25, 2023"
-        nextText="Jan 26, 2023"
-      />
-      <div className="page-content">
-        <CardGrid
-          heading={
-            <h1>
-              10 Most <br />
-              <span>Popular</span> Models
-            </h1>
-          }
-          cards={[
-            {
-              icon: "/models/mistral.webp",
-              modelAuthor: "stabilityai",
-              modelName: "japanese-stablelm-instruct-alpha-7b",
-              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-            },
-            {
-              icon: "/models/mistral.webp",
-              modelAuthor: "mistralai",
-              modelName: "Mistral-7B-v0.1",
-              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-            },
-            {
-              icon: "/models/mistral.webp",
-              modelAuthor: "mistralai",
-              modelName: "Mistral-7B-v0.1",
-              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-            },
-            {
-              icon: "/models/mistral.webp",
-              modelAuthor: "mistralai",
-              modelName: "Mistral-7B-v0.1",
-              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-            },
-            {
-              icon: "/models/mistral.webp",
-              modelAuthor: "mistralai",
-              modelName: "Mistral-7B-v0.1",
-              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-            },
-            {
-              icon: "/models/mistral.webp",
-              modelAuthor: "mistralai",
-              modelName: "Mistral-7B-v0.1",
-              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-            },
-            {
-              icon: "/models/mistral.webp",
-              modelAuthor: "mistralai",
-              modelName: "Mistral-7B-v0.1",
-              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-            },
-            {
-              icon: "/models/mistral.webp",
-              modelAuthor: "mistralai",
-              modelName: "Mistral-7B-v0.1",
-              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-            },
-            {
-              icon: "/models/mistral.webp",
-              modelAuthor: "mistralai",
-              modelName: "Mistral-7B-v0.1",
-              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-            },
-            {
-              icon: "/models/mistral.webp",
-              modelAuthor: "mistralai",
-              modelName: "Mistral-7B-v0.1",
-              externalLink: "https://huggingface.co/mistralai/Mistral-7B-v0.1",
-            },
-          ]}
-        />
-        <Switchback
-          leftContent={
-            <h2 className="heading">
-              <span>45,515</span> text gen
-              <br /> models existed
-            </h2>
-          }
-          rightContent={
-            <p>
-              We like to say that in AI, a week is a month and a month is a
-              year, and the "latest and greatest model" changes just as quickly.
-              We tracked the most popular text-gen models starting in January
-              2023 to highlight the rapid rate of change in the AI industry.
-              Click through time to see how quickly the state-of-the-art
-              changes.
-            </p>
-          }
-        />
-      </div>
+    <main className="container not-found">
+      <h1 className="heading">
+        Page <span>not found</span>
+      </h1>
+      <a href="/" className="button">
+        Go back home!
+      </a>
     </main>
   );
 }
